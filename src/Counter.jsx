dcad5c2e3022cf9b8dfdd2125be407e0c543ec0b
Dušan Rosić <dusanrosic25.06.1997@gmail.com>
2024-04-24 00:35:16 +0200

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+function Counter() {
+	const [count, setCount] = useState();
+
+	const increment = () => {
+		setCount(count + 1);
+	}
+
+	const decrement = () => {
+		setCount(count - 1);
+	}
+
+	const reset = () => {
+		setCount(0);
+	}
+
+	return(<div className="counter-container">
+		<p className="count-display">Count: {count}</p>
+		<button className="counter-button-increment" onClick={increment}>Increment (+)</button>
+		<button className="counter-button-decrement" onClick={decrement}>Decrement (-)</button>
+		<button className="counter-button-reset" onClick={reset}>Reset (0)</button>
+
+	</div>);
+}
+
+export default Counter
