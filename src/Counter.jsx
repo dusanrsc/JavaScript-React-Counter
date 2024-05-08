803b7c254eb84f7bad20cccc1074e08d49a52c28
Dušan Rosić SRB <149257819+dusanrsc@users.noreply.github.com>
2024-05-08 13:53:21 +0200

    Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 function Counter() {
-	const [count, setCount] = useState();
+	const [count, setCount] = useState(0);
 
 	const increment = () => {
 		setCount(count + 1);
